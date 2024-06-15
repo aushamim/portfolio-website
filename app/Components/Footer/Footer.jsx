9004b1a301c409118714987adff1864a5175f54b
aushamim <au.shamim0@gmail.com>
2024-06-15 17:23:01 +0600

    [Fix] Footer

--- app/Components/Footer/Footer.jsx
+++ app/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-white p-5 font-medium text-center border-t">
+    <div className="bg-white text-xs xl:text-base p-2 xl:p-5 font-medium text-center border-t">
       ©️ 2024 All Copyright Reserved - Shamim
     </div>
   );
